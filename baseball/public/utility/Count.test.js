208e200d7083f6124ee42f4b987c5f9a3639d24c
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-08 13:54:39 -0600

    writing unit test for strike function

--- baseball/public/utility/Count.test.js
+++ baseball/public/utility/Count.test.js
@@ -1,1 +1,10 @@
 import { strike } from './Count'
+import { italic } from 'ansi-colors';
+
+describe('Count.js', () => {
+    describe('strike()', () => {
+        it('When strike button is clicked it should add one to current state unless current state is 3 then it should reset to zero', () => {
+            expectExport()
+        })
+    })
+})
