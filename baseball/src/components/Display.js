3ea4762a66af9666f77938bac748ea5b3407270f
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-08 13:41:39 -0600

    build out Display and Dashboard a bit more

--- baseball/src/components/Display.js
+++ baseball/src/components/Display.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import Dashboard from './Dashboard'
+
+// this component will hold state
+
+const Display  = () => {
+    return (
+        <div>
+            <h1>From Display.js</h1>
+            <Dashboard />
+        </div>
+    )
+}
+
+export default Display
